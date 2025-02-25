72d590db4bbe670775f8c4c86223f5be0d39b73f
Tetyana_Dmytryienko <dmitrienko_tanya@ukr.net>
2025-02-25 14:47:02 +0200

    react hw2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
-function App() {
-  const [clicks, setClicks] = useState(0);
+
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (savedFeedback) {
+      setFeedback(savedFeedback);
+    }
+  }, []);
+
+  
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => {
+      const newFeedback = { ...prevFeedback, [feedbackType]: prevFeedback[feedbackType] + 1 };
+      localStorage.setItem("feedback", JSON.stringify(newFeedback)); // Збереження в локальному сховищі
+      return newFeedback;
+    });
+  };
+
+ 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  
+  const positiveFeedback = totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
   return (
-    <div>
-      <Description />
-      <Options />
+    <div className="App">
+      <Description/>
+      
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} positiveFeedback={positiveFeedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
-}
+};
+
+
+
+
 
 export default App;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-const Options = () => {
-  let clicks = 0;
-  const handleClick = () => {
-    clicks = clicks + 1;
-  };
-
+const Options = ({ updateFeedback, totalFeedback }) => {
   return (
     <div>
-      <button onClick={handleClick}>Good: {clicks}</button>
-      <button onClick={(evt) => console.log(evt)}>Second button</button>
+      <button onClick={() => updateFeedback("good")}>Good</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button onClick={() => updateFeedback("bad")}>Bad</button>
+
+      {totalFeedback > 0 && (
+        <button onClick={() => {
+          localStorage.removeItem("feedback");
+          window.location.reload(); // Перезавантажуємо сторінку після скидання
+        }}>Reset</button>
+      )}
     </div>
   );
 };
-
 export default Options;
