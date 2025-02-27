59f5e5c5d362f8d87c0a4e9faf264564be8542f3
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-02-27 13:45:51 +0200

    correct error

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,58 @@ import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
-
-
 const App = () => {
   const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   });
 
-  
   useEffect(() => {
     const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
     if (savedFeedback) {
       setFeedback(savedFeedback);
     }
   }, []);
+  useEffect(() => {
+    if (feedback) {
+      localStorage.setItem("feedback", JSON.stringify(feedback));
+    }
+  }, [feedback]);
 
-   
   const onLeaveFeedback = (option) => {
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
-      [option]: prevFeedback[option] + 1
+      [option]: prevFeedback[option] + 1,
     }));
   };
- 
+  const handleReset = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+    localStorage.removeItem("feedback");
+  };
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback = totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   return (
     <div className="App">
-      <Description/>
-       <Options onLeaveFeedback={onLeaveFeedback} totalFeedback={totalFeedback} />
+      <Description />
+      <Options
+        onLeaveFeedback={onLeaveFeedback}
+        totalFeedback={totalFeedback}
+        onReset={handleReset}
+      />
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} positiveFeedback={positiveFeedback} totalFeedback={totalFeedback} />
+        <Feedback
+          feedback={feedback}
+          positiveFeedback={positiveFeedback}
+          totalFeedback={totalFeedback}
+        />
       ) : (
         <Notification />
       )}
     </div>
   );
 };
-
-
-
-
 
 export default App;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,18 +1,13 @@
-import PropTypes from 'prop-types';
-import css from "./Options.module.css"
-const Options = ({ onLeaveFeedback, totalFeedback }) => {
+import PropTypes from "prop-types";
+import css from "./Options.module.css";
+const Options = ({ onLeaveFeedback, totalFeedback, onReset }) => {
   return (
     <div className={css.btnOptions}>
       <button onClick={() => onLeaveFeedback("good")}>Good</button>
       <button onClick={() => onLeaveFeedback("neutral")}>Neutral</button>
       <button onClick={() => onLeaveFeedback("bad")}>Bad</button>
 
-      {totalFeedback > 0 && (
-        <button onClick={() => {
-          localStorage.removeItem("feedback");
-          window.location.reload(); 
-        }}>Reset</button>
-      )}
+      {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
     </div>
   );
 };
